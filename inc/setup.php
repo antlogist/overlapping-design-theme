db5ce106e00391abb632d5333d80f317b50bd722
antlogist <antlogist@gmail.com>
2021-09-17 20:58:04 +0300

    Image sizes added

--- inc/setup.php
+++ inc/setup.php
@@ -9,8 +9,14 @@ function overlap_theme_support() {
   //Thumbnails support
   add_theme_support("post-thumbnails");
   
+  //Image size
+  add_image_size("pageBanner", 1500, 350, true);
+  add_image_size("standardImage", 1024, 683, true);
+  add_image_size("squareImage", 683, 683, true);
+  
   //Document title tag support
   add_theme_support( "title-tag" );
   
+  //Menus
   add_theme_support("menus");
 }
