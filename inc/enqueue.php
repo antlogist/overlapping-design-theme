4da519d38e320684ce39f8dadd8d579208709d43
antlogist <antlogist@gmail.com>
2021-09-18 21:06:48 +0300

    Real time tagline updation added

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -7,11 +7,16 @@ if ( ! defined ('ABSPATH') ) {
 
 function overlap_styles_and_scripts() {
   //libs CSS
-  wp_enqueue_style("overlap-lib-css", get_theme_file_uri("dist/css/libs.css"));  
+  wp_enqueue_style('overlap-lib-css', get_theme_file_uri('dist/css/libs.css'));  
     
   //custom CSS
-  wp_enqueue_style("overlap-app-css", get_theme_file_uri("dist/css/all.css"), NULL, microtime());
+  wp_enqueue_style('overlap-app-css', get_theme_file_uri('dist/css/all.css'), NULL, microtime());
   
   //custom js
-  wp_enqueue_script("overlap-app-js", get_theme_file_uri("dist/js/all.js"), null, microtime(), true);
+  wp_enqueue_script('overlap-app-js', get_theme_file_uri('dist/js/all.js'), null, microtime(), true);
+}
+
+function overlap_customizer_script() {
+  //custom js
+  wp_enqueue_script('overlap-customizer-js', get_theme_file_uri('dist/js/theme-customize.js'), array( 'jquery','customize-preview' ), microtime(), true);
 }
