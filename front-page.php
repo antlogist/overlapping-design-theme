1de6c6416deb2c177497252a28db3a29f03b56ca
antlogist <antlogist@gmail.com>
2021-09-18 09:21:52 +0300

    Button styles added

--- front-page.php
+++ front-page.php
@@ -7,4 +7,8 @@ if ( ! defined ("ABSPATH") ) {
 
 get_header();
 echo "<h1>Front Page</h1>";
+echo "<a class='_btn'>Button</a><br>";
+echo "<a class='_btn _btn-invert'>Button</a><br>";
+echo "<button class='_btn'>Button</button><br>";
+echo "<button class='_btn _btn-invert'>Button</button>";
 get_footer();
