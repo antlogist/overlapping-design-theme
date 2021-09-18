d4b09ab4f4174e4a5a0be631dfbf8af1b28738be
antlogist <antlogist@gmail.com>
2021-09-18 13:38:01 +0300

    Frontpage content template added

--- front-page.php
+++ front-page.php
@@ -22,10 +22,40 @@ get_header(); ?>
 </div>
 <!--/Carousel-->
 
-<?php
-echo "<h1>Front Page</h1>";
-echo "<a class='_btn'>Button</a><br>";
-echo "<a class='_btn _btn-invert'>Button</a><br>";
-echo "<button class='_btn'>Button</button><br>";
-echo "<button class='_btn _btn-invert'>Button</button>";
-get_footer();
+<!--Content-->
+<div class="figures-wrapper">
+  <div class="figure figure-1"></div>
+  <div class="figure figure-2">
+    <div class="logo-wrapper d-flex align-items-center justify-content-center">
+      <div>
+        <img src="https://dummyimage.com/512x512/ffbd00/000.jpg" class="logo" alt="logo">
+        <h2 class="tagline text-uppercase">Lorem Ipsum<br>Dolor sit Amet</h2>
+        <div class="button-wrapper">
+          <button class="_btn _btn-left _btn-invert" id="openModalAbout">About Us</button>
+          <button class="_btn _btn-right mt-3" id="openModalProducts">Products</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="figure figure-3">
+    <div class="contact-wrapper d-flex align-items-center">
+      <div class="mt-3">
+        <h3 class="tagline text-uppercase">Call Us</h3>
+        <h5 class="contact">Phone: <a href="tel:1247 5678">1247 5678</a></h5>
+
+        <h3 class="tagline text-uppercase mt-4">Address</h3>
+        <h5 class="contact">251 Beibitshilik Street, New Chum QLD 1234</h5>
+
+        <h3 class="tagline text-uppercase mt-4">Postal Address</h3>
+        <h5 class="contact">251 Beibitshilik Street, New Chum QLD 1234</h5>
+        <div class="button-wrapper">
+          <button class="_btn _btn-left _btn-invert mt-3" id="openModalMail">Send Mail</button>
+          <button class="_btn _btn-right mt-3" id="openModalMap">Open Map</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!--/Content-->
+
+<?php get_footer(); ?>
