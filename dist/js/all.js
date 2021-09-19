518b12dccdf435d6ef9a7d052b146bcf0bbe94e5
antlogist <antlogist@gmail.com>
2021-09-19 12:26:21 +0300

    buttons js added

--- dist/js/all.js
+++ dist/js/all.js
@@ -1,6 +1,330 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./resources/assets/js/Classes/Mail.js":
+/*!*********************************************!*\
+  !*** ./resources/assets/js/Classes/Mail.js ***!
+  \*********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ Mail)
+/* harmony export */ });
+/* harmony import */ var _Request_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Request.js */ "./resources/assets/js/Classes/Request.js");
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+
+
+var Mail = /*#__PURE__*/function () {
+  function Mail() {
+    _classCallCheck(this, Mail);
+
+    this.headers = {
+      "Content-Type": "application/x-www-form-urlencoded"
+    };
+    this.url = "".concat(baseUrl, "wp-content/themes/overlapping-design-theme/inc/mail/mail.php");
+  }
+
+  _createClass(Mail, [{
+    key: "getHeaders",
+    value: function getHeaders() {
+      return this.headers;
+    }
+  }, {
+    key: "sendMail",
+    value: function sendMail(formData) {
+      var id = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "";
+      var myHttp = new _Request_js__WEBPACK_IMPORTED_MODULE_0__["default"]();
+      var body = formData;
+      myHttp.post(this.url, body, this.headers, function (err, resp) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+
+        console.log(resp);
+        var message = document.getElementById(id);
+        var parent = message.parentElement;
+        parent.classList.add("w-100", "h-100", "d-flex");
+        parent.classList.remove("pt-5", "mt-5");
+        message.innerHTML = "";
+        var template = "\n        <div class=\"d-flex align-items-center justify-content-center h-100 w-100\">\n          <h1>".concat(resp.message, "</h1>\n        </div>\n        ");
+        message.insertAdjacentHTML("afterbegin", template);
+      });
+    }
+  }]);
+
+  return Mail;
+}();
+
+
+
+/***/ }),
+
+/***/ "./resources/assets/js/Classes/Modal.js":
+/*!**********************************************!*\
+  !*** ./resources/assets/js/Classes/Modal.js ***!
+  \**********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ Modal)
+/* harmony export */ });
+/* harmony import */ var _Mail_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Mail.js */ "./resources/assets/js/Classes/Mail.js");
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+
+
+var Modal = /*#__PURE__*/function () {
+  function Modal() {
+    _classCallCheck(this, Modal);
+
+    this.body = document.body;
+    this.fragment = document.createDocumentFragment();
+    this.modalId = "modalWindow";
+    this.modalClass = "_modal";
+    this.overlayClass = "_modal-overlay";
+    this.buttonCloseId = "modalClose";
+    this.buttonCloseIdBottom = "modalCloseBottom";
+    this.buttonSendMail = "buttonSendMail";
+  }
+
+  _createClass(Modal, [{
+    key: "renderTemplate",
+    value: function renderTemplate(htmlTemplate) {
+      var modal = document.getElementById(this.modalId);
+
+      if (modal) {
+        console.log("Element already exists!");
+        return;
+      }
+
+      this.body.classList.add("active");
+      this.fragment.appendChild(this.createOverlay());
+      this.fragment.appendChild(this.createTemplate(htmlTemplate));
+      this.body.appendChild(this.fragment);
+      this.listenClick();
+
+      if (!!document.querySelector("._modal .needs-validation")) {
+        this.formValidation();
+      }
+
+      this.classAnimation();
+    }
+  }, {
+    key: "formValidation",
+    value: function formValidation() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var modalForm = document.querySelector("._modal .needs-validation");
+      var valid = false; // Loop over them and prevent submission
+
+      modalForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        valid = false;
+
+        if (!modalForm.checkValidity()) {
+          event.stopPropagation();
+          valid = false;
+        } else {
+          valid = true;
+        }
+
+        modalForm.classList.add("was-validated");
+
+        if (valid) {
+          var formData = new FormData(modalForm);
+          var data = {
+            name: formData.get("name"),
+            phone: formData.get("phone"),
+            email: formData.get("email"),
+            suburb: formData.get("suburb"),
+            message: formData.get("message"),
+            token: token
+          };
+          var mail = new _Mail_js__WEBPACK_IMPORTED_MODULE_0__["default"]();
+          mail.sendMail(new URLSearchParams(data).toString(), "messageContainer");
+        }
+      }, false);
+    }
+  }, {
+    key: "createOverlay",
+    value: function createOverlay() {
+      var overlay = document.createElement("div");
+      overlay.classList.add(this.overlayClass);
+      return overlay;
+    }
+  }, {
+    key: "createTemplate",
+    value: function createTemplate(htmlTemplate) {
+      //Modal
+      var modal = document.createElement("div");
+      modal.classList.add(this.modalClass);
+      modal.id = this.modalId; //HTML template
+
+      modal.insertAdjacentHTML("afterBegin", "<div class=\"_modal-wrapper\"><a href=\"#\" class=\"_close-modal-button\" id=\"".concat(this.buttonCloseId, "\"><span class=\"toggle-line toggle-line-1\"></span><span class=\"toggle-line toggle-line-2\"></span></a><div class=\"_modal-content-wrapper h-100 w-100\">").concat(htmlTemplate, "</div></div>"));
+      return modal;
+    }
+  }, {
+    key: "listenClick",
+    value: function listenClick() {
+      var modal = document.getElementById(this.modalId);
+      var overlay = document.querySelector(".".concat(this.overlayClass));
+      var buttonCloseId = this.buttonCloseId;
+      var buttonCloseIdBottom = this.buttonCloseIdBottom;
+      var body = this.body;
+      modal.addEventListener("click", function (e) {
+        //Close button click
+        if (buttonCloseId === e.target.id || buttonCloseId === e.target.parentElement.id || buttonCloseIdBottom === e.target.id) {
+          e.preventDefault();
+          modal.remove();
+          overlay.remove();
+          body.classList.remove("active");
+        }
+      });
+    }
+  }, {
+    key: "classAnimation",
+    value: function classAnimation() {
+      var modal = document.getElementById(this.modalId);
+      setTimeout(function () {
+        modal.classList.add("active");
+      }, 250);
+    }
+  }]);
+
+  return Modal;
+}();
+
+
+
+/***/ }),
+
+/***/ "./resources/assets/js/Classes/Request.js":
+/*!************************************************!*\
+  !*** ./resources/assets/js/Classes/Request.js ***!
+  \************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ Request)
+/* harmony export */ });
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Request = /*#__PURE__*/function () {
+  function Request() {
+    _classCallCheck(this, Request);
+  }
+
+  _createClass(Request, [{
+    key: "get",
+    value: //Get
+    function get(url, cb) {
+      try {
+        // xhr object
+        var xhr = new XMLHttpRequest(); // xhr open
+
+        xhr.open("GET", url); // xhr load
+
+        xhr.addEventListener("load", function () {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb("Error. Status code ".concat(xhr.status), xhr);
+            return;
+          } // parse
+
+
+          var response = JSON.parse(xhr.responseText); // callback
+
+          cb(null, response);
+        }); // xhr error
+
+        xhr.addEventListener("error", function () {
+          console.log("error");
+        }); // xhr send
+
+        xhr.send();
+      } catch (error) {
+        console.log(error);
+      }
+    } //Post
+
+  }, {
+    key: "post",
+    value: function post(url, body, headers, cb) {
+      // xhr object
+      var xhr = new XMLHttpRequest(); // xhr open
+
+      xhr.open("POST", url); // headers
+
+      if (headers) {
+        Object.entries(headers).map(function (_ref) {
+          var _ref2 = _slicedToArray(_ref, 2),
+              key = _ref2[0],
+              value = _ref2[1];
+
+          xhr.setRequestHeader(key, value);
+        });
+      } // xhr load
+
+
+      xhr.addEventListener("load", function () {
+        if (Math.floor(xhr.status / 100) !== 2) {
+          cb("Error. Status code ".concat(xhr.status), xhr);
+          return;
+        } // parse
+
+
+        var response = JSON.parse(xhr.responseText); // callback
+
+        cb(null, response);
+      }); // xhr error
+
+      xhr.addEventListener("error", function () {
+        console.log("error");
+      }); // xhr send
+      //    console.log(JSON.stringify(body));
+      //    xhr.send(JSON.stringify(body));
+
+      xhr.send(body);
+    }
+  }]);
+
+  return Request;
+}();
+
+
+
+/***/ }),
+
 /***/ "./resources/assets/libs/bootstrap/js/dist/base-component.js":
 /*!*******************************************************************!*\
   !*** ./resources/assets/libs/bootstrap/js/dist/base-component.js ***!
@@ -1819,6 +2143,35 @@ __webpack_require__.r(__webpack_exports__);
 })();
 })();
 
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+var __webpack_exports__ = {};
+/*!************************************************!*\
+  !*** ./resources/assets/js/buttons/buttons.js ***!
+  \************************************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _Classes_Modal_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../Classes/Modal.js */ "./resources/assets/js/Classes/Modal.js");
+
+
+(function () {
+  "use strict";
+
+  BASEOBJECT.buttons.init = function () {
+    var body = document.body;
+    var modal = new _Classes_Modal_js__WEBPACK_IMPORTED_MODULE_0__["default"]();
+    var template = "";
+    body.addEventListener("click", function (e) {
+      if (e.target.id === "openModalAbout") {
+        template = "";
+        template = "\n          <div class=\"container-fluid container-xxl\">\n            <div class=\"paragraphs-wrapper py-5\">\n              <h2 class=\"_modal-title\">About Us</h2>\n              Content...\n              <div class=\"buttons-wrapper w-100 mt-3\">\n                <a href=\"./about.php\" class=\"_btn d-inline-block text-uppercase\">More...</a>\n                <a href=\"#\" class=\"_btn _btn-invert d-inline-block text-uppercase\" id=\"modalCloseBottom\">Close</a>\n              </div>\n            </div>\n          </div>";
+        modal.renderTemplate(template);
+      }
+    });
+  };
+})();
+})();
+
 // This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
 (() => {
 /*!*************************************!*\
@@ -1828,6 +2181,7 @@ __webpack_require__.r(__webpack_exports__);
   "use strict";
 
   BASEOBJECT.nav.toggleButton();
+  BASEOBJECT.buttons.init();
   var body = document.body;
 
   switch (body.id) {
